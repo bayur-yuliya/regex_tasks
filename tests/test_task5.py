7a5174439a4c90eeb054f335cea98265cb910599
Yuliya Bayur <juliy14497@gmail.com>
2024-01-25 21:27:43 +0200

    Change test by task5

--- tests/test_task5.py
+++ tests/test_task5.py
@@ -8,10 +8,8 @@ def test_split_sentences():
     assert split_sentences(example_text) == ["You can't parse [X]HTML with regex.",
                  "Because HTML can't be parsed by regex.",
                  'Regex is not a tool that can be used to correctly parse HTML.',
-                 'As I have answered in HTML-and-regex questions here so many times before, '
-                 'the use of regex will not allow you to consume HTML.',
-                 'Regular expressions are a tool that is insufficiently sophisticated to '
-                 'understand the constructs employed by HTML.']
+                 'As I have answered in HTML-and-regex questions here so many times before, the use of regex will not allow you to consume HTML.',
+                 'Regular expressions are a tool that is insufficiently sophisticated to understand the constructs employed by HTML.']
     assert split_sentences(example_text1) == ['Это предложение номер один.',
                  'И вот предложение номер два!',
                  'А это третье?',
